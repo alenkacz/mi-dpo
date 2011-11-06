89eba3a598e75bd2ba801bd75b28c4fac1b44344
alenkacz <varkockova.a@gmail.com>
2011-11-06 20:21:37 +0100

    square and circle table, dummy adapter added

---
+++ MVC/src/cvut/fit/dpo/mvc/view/ShapeAdapter.java
@@ -0,0 +1,15 @@
+package cvut.fit.dpo.mvc.view;
+
+import cvut.fit.dpo.mvc.model.Shape2d;
+
+public class ShapeAdapter {
+	public Object[] getTableData(Shape2d s) {
+		String[] result = new String[4];
+		result[0] = String.valueOf(s.getId());
+		result[1] = String.valueOf(s.getPoint().x);
+		result[2] = String.valueOf(s.getPoint().y);
+		result[3] = String.valueOf(s.getX());
+		
+		return result;
+	}
+}
--- MVC/src/cvut/fit/dpo/mvc/view/TableView.java
+++ MVC/src/cvut/fit/dpo/mvc/view/TableView.java
@@ -2,6 +2,7 @@ package cvut.fit.dpo.mvc.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -12,6 +13,8 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 
 import cvut.fit.dpo.mvc.controller.Controller;
+import cvut.fit.dpo.mvc.model.Circle;
+import cvut.fit.dpo.mvc.model.Shape2d;
 import cvut.fit.dpo.mvc.model.ShapeModel;
 
 public class TableView extends View implements Observer{
@@ -20,20 +23,53 @@ public class TableView extends View implements Observer{
 	private static final String[] tableHeaderCircle = {"id", "x", "y", "r"};
 	
 	JButton clearButton;
+	ShapeTableModel squareTableModel;
+	ShapeTableModel circleTableModel;
+	ShapeAdapter shapeAdapter = new ShapeAdapter();
 	
 	@Override
 	public JPanel getView() {
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));	
 		clearButton = new JButton("Clear all");
-		JTable table = new JTable();
-		ShapeTableModel model = new ShapeTableModel();
-		model.setColumnIdentifiers(tableHeaderSquare);
-		table.setModel(model);
 		
-		add(table);
+		add(createSquareTable());
+		add(createCircleTable());
 		add(clearButton);
+		fillData();
+		
 		return this;
+	}
+	
+	private JTable createSquareTable() {
+		JTable table = new JTable();
+		table.setPreferredSize(new Dimension(200,100));
+		squareTableModel = new ShapeTableModel();
+		squareTableModel.setColumnIdentifiers(tableHeaderSquare);
+		table.setModel(squareTableModel);
+		
+		return table;
+	}
+	
+	private JTable createCircleTable() {
+		JTable table = new JTable();
+		table.setPreferredSize(new Dimension(200,100));
+		circleTableModel = new ShapeTableModel();
+		circleTableModel.setColumnIdentifiers(tableHeaderCircle);
+		table.setModel(circleTableModel);
+		
+		return table;
+	}
+	
+	private void fillData() {
+		try {
+			Shape2d s = new Circle(new Point(10,20));
+			squareTableModel.addRow(shapeAdapter.getTableData(s));
+			squareTableModel.addRow(shapeAdapter.getTableData(s));
+			squareTableModel.addRow(shapeAdapter.getTableData(s));
+			circleTableModel.addRow(shapeAdapter.getTableData(s));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -45,7 +81,6 @@ public class TableView extends View implements Observer{
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
 	}
 
 }
