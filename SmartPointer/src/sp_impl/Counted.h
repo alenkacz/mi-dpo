666f53eafdd2fcbb972a682d47ae4ccec91295e9
alenkacz <varkockova.a@gmail.com>
2011-12-01 18:13:15 +0100

    gitignore changes

---
+++ SmartPointer/src/sp_impl/Counted.h
@@ -0,0 +1,17 @@
+/*
+ * Counted.h
+ *
+ *  Created on: Nov 20, 2011
+ *      Author: Alena Varkockova (varkockova.a@gmail.com)
+ */
+
+#ifndef COUNTED_H_
+#define COUNTED_H_
+
+class Counted {
+public:
+	Counted();
+	virtual ~Counted();
+};
+
+#endif /* COUNTED_H_ */
